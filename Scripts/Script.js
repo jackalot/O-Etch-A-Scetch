4896db25a5f972ea0b4aca38c170139ddba279c8
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-08 11:39:23 -0400

    div with id + i gets appended to container

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -2,3 +2,11 @@ const container = document.createElement("div");
 container.id = "container";
 const body = document.querySelector("body");
 body.append(container);
+//Row 1
+for(i = 1; i < 16; i++)
+{
+    const div = document.createAttribute("div");
+    div.id = "1-" + i;
+    div.classList.add('box');
+    container.append(div);
+}
