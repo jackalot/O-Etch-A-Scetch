7ef566299ecfbe4c3a07c7ad700ebabe96a732e7
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-08 11:59:00 -0400

    made the first row

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -3,10 +3,10 @@ container.id = "container";
 const body = document.querySelector("body");
 body.append(container);
 //Row 1
-for(i = 1; i < 16; i++)
+for(i=1; i <= 16; i++)
 {
-    const div = document.createAttribute("div");
-    div.id = "1-" + i;
-    div.classList.add('box');
-    container.append(div);
+    let newdiv = document.createElement("div");
+    newdiv.id = "1-" + i;
+    container.appendChild(newdiv);  
 }
+    
