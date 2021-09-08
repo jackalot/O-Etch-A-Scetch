b1b31e61d752a567592221026d7f7ecb37e817f5
jackalotUVM <gonzalezc9999@gmail.com>
2021-09-08 12:02:40 -0400

    made all rows

--- Scripts/Script.js
+++ Scripts/Script.js
@@ -7,6 +7,111 @@ for(i=1; i <= 16; i++)
 {
     let newdiv = document.createElement("div");
     newdiv.id = "1-" + i;
+    newdiv.classList.add("box");
     container.appendChild(newdiv);  
 }
-    
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "2-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "3-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "4-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "5-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "6-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "7-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "8-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "9-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "10-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "11-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "12-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "13-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "14-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "15-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
+for(i=1; i <= 16; i++)
+{
+    let newdiv = document.createElement("div");
+    newdiv.id = "16-" + i;
+    newdiv.classList.add("box");
+    container.appendChild(newdiv);  
+}
